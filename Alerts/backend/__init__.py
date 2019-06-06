f2f14ac1f111e1f1cf89ea65d0186087538369f4
Akhady <melicharada@seznam.cz>
2019-06-06 15:04:12 +0200

    backend ADD records functions

---
+++ Alerts/backend/__init__.py
@@ -0,0 +1,30 @@
+from liberouterapi import config
+from liberouterapi.dbConnector import dbConnector
+from liberouterapi.modules.module import Module
+
+# Load Alerts configuration file if Alerts section is not present in current config
+if "alerts" not in config.config.sections():
+    config.load(path = __path__[0] + '/config.ini')
+    conf_path = config['alerts']
+
+# We need collection for Alerts to be set up
+alerts_conn = dbConnector("alerts", provider="mongodb", config={'database': config['alerts']['database']})
+alerts_coll = alerts_conn.db[config['alerts']['collection']]
+
+# Register a blueprint
+alerts_bp = Module('alerts', __name__, url_prefix='/alerts', no_version=True)
+
+from .fill_db import *
+from .records_loader import *
+
+# pomocne funkce pro docasne testovani
+# fill_db_with_data()
+# print(get_number_of_all_records())
+# print(get_limited_number_of_records(2, 3))
+# delete_data_from_db()
+
+# Get number of all records in database
+alerts_bp.add_url_rule('/alert-count', view_func=get_number_of_all_records, methods=['GET'])
+
+# Get limited number of records for one page
+alerts_bp.add_url_rule('/alert-page', view_func=get_limited_number_of_records, methods=['GET'])
