03980d7b71afba22bf5300fe2d5646dd81f79fdf
Akhady <melicharada@seznam.cz>
2019-06-07 10:41:55 +0200

    backend FIX alert table status update

--- Alerts/backend/records_loader.py
+++ Alerts/backend/records_loader.py
@@ -11,7 +11,7 @@ alerts_db = dbConnector("alerts")
 alerts_coll = alerts_db.db[config['alerts']['collection']]
 
 
-# @auth.required()
+@auth.required()
 def get_number_of_all_records():
     number_of_records = alerts_coll.find().count()
     return json.dumps(number_of_records)
@@ -24,8 +24,8 @@ def get_limited_number_of_records():
     first_item = items * (page - 1)
 
     ids = [x["_id"] for x in list(alerts_coll.find({}, {"_id": 1}).skip(first_item).limit(items))]
-    alerts_coll.update_many({"_id": {"$in": ids}, "status": {"$exists": False}}, {"$set": {"Status": 3}})
-    alerts_coll.update_many({"_id": {"$in": ids}, "status": 3}, {"$set": {"Status": 0}})
+    alerts_coll.update_many({"_id": {"$in": ids}, "Status": 3}, {"$set": {"Status": 0}})
+    alerts_coll.update_many({"_id": {"$in": ids}, "Status": {"$exists": False}}, {"$set": {"Status": 3}})
 
     query = {"_id": 0, "DetectTime": 1, "Category": 1, "Source": 1, "Target": 1, "FlowCount": 1, "Status": 1, "ID": 1}
     records = list(alerts_coll.find({}, query).skip(first_item).limit(items))
