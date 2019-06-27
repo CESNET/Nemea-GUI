8a28c2b513d8a82c594f84fef7dd9202f2dfd2ee
Akhady <melicharada@seznam.cz>
2019-06-27 14:42:42 +0200

    backend ADD function to remove filter from database

--- Alerts/backend/filters.py
+++ Alerts/backend/filters.py
@@ -50,3 +50,17 @@ def get_filter_names():
     records = list(filters_coll.aggregate([{'$group': {'_id': user, 'names': {'$push': '$name'}}},
                                            {'$project': {'_id': 0, 'names': 1}}]))
     return json.dumps(records)
+
+
+@auth.required()
+def delete_filter():
+    data = request.json
+    name = data['name']
+    session = auth.lookup(request.headers.get('lgui-Authorization', None))
+    user = session['user'].username
+
+    try:
+        filters_coll.delete_one({'name': name, 'user': user})
+        return json.dumps({"success": True, "errCode": 200})
+    except Exception:
+        return json.dumps({"success": False, "errCode": 500})
